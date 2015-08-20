b6d128bda7700d36eca459692e56c8fd815f01d0
Jason Paryani <github@jparyani.com>
2015-08-20 13:31:37 -0700

    Many php apps break without a properly created sessions directory

--- stacks/lemp/launcher.sh
+++ stacks/lemp/launcher.sh
@@ -3,6 +3,7 @@
 # Create a bunch of folders under the clean /var that php, nginx, and mysql expect to exist
 mkdir -p /var/lib/mysql
 mkdir -p /var/lib/nginx
+mkdir -p /var/lib/php5/sessions
 mkdir -p /var/log
 mkdir -p /var/log/mysql
 mkdir -p /var/log/nginx
