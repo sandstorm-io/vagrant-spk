23259dee437f06a8945c032d9eab2ba5883a0c4a
Asheesh Laroia <asheesh@asheesh.org>
2016-01-22 23:57:59 -0800

    rm -f, for idempotence

--- stacks/lemp/setup.sh
+++ stacks/lemp/setup.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y nginx php5-fpm php5-mysql php5-cli php5-curl git php5-dev mysql-server
-unlink /etc/nginx/sites-enabled/default
+rm -f /etc/nginx/sites-enabled/default
 cat > /etc/nginx/sites-available/sandstorm-php <<EOF
 server {
     listen 8000 default_server;
