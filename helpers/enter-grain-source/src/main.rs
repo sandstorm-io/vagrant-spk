eb4b475af26d948c4221ba238ed173c3471c22ee
Asheesh Laroia <asheesh@sandstorm.io>
2016-05-25 18:18:02 -0700

    Remove one unused variable

--- helpers/enter-grain-source/src/main.rs
+++ helpers/enter-grain-source/src/main.rs
@@ -94,7 +94,6 @@ fn get_envp(pidStrRef: &str) -> std::vec::Vec<*const u8> {
     // at the end, we can provide the environment.
 
     let path = "/proc/".to_string() + pidStrRef + &"/environ".to_string();
-    let mut envp: &[*const u8];
     let mut file = match File::open(&path) {
         Err(why) => panic!("couldn't open {}: {}", path, why),
         Ok(mut file) => {
