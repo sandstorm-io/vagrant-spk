c492528cfb23b6f1cfdd67152d12e353e2aba6ef
Guillaume Rivier <rivier@adacore.com>
2018-03-01 00:29:44 +0100

    Update php version in other files.

--- stacks/lemp/launcher.sh
+++ stacks/lemp/launcher.sh
@@ -3,7 +3,7 @@
 # Create a bunch of folders under the clean /var that php, nginx, and mysql expect to exist
 mkdir -p /var/lib/mysql
 mkdir -p /var/lib/nginx
-mkdir -p /var/lib/php5/sessions
+mkdir -p /var/lib/php/sessions
 mkdir -p /var/log
 mkdir -p /var/log/mysql
 mkdir -p /var/log/nginx
@@ -20,14 +20,14 @@ HOME=/etc/mysql /usr/bin/mysql_install_db --force
 
 # Spawn mysqld, php
 HOME=/etc/mysql /usr/sbin/mysqld &
-/usr/sbin/php5-fpm --nodaemonize --fpm-config /etc/php5/fpm/php-fpm.conf &
+/usr/sbin/php-fpm7.0 --nodaemonize --fpm-config /etc/php/7.0/fpm/php-fpm.conf &
 # Wait until mysql and php have bound their sockets, indicating readiness
 while [ ! -e /var/run/mysqld/mysqld.sock ] ; do
     echo "waiting for mysql to be available at /var/run/mysqld/mysqld.sock"
     sleep .2
 done
-while [ ! -e /var/run/php5-fpm.sock ] ; do
-    echo "waiting for php5-fpm to be available at /var/run/php5-fpm.sock"
+while [ ! -e /var/run/php/php7.0-fpm.sock ] ; do
+    echo "waiting for php-fpm7.0 to be available at /var/run/php/php7.0-fpm.sock"
     sleep .2
 done
 
