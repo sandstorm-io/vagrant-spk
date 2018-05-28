b873268d288ed994d9269518fddd0d76aa9e6319
Jacob Weisz <inbox@jacobweisz.com>
2018-05-28 18:29:16 -0500

    Update Go stack
    Fixes #232. (I think)
    
    Can confirm this change causes it to install golang-1.10, but don't have anything to test against handy.

--- stacks/golang/setup.sh
+++ stacks/golang/setup.sh
@@ -8,10 +8,10 @@ set -euo pipefail
 # The version of golang in the main repo is *ancient* (1.3.x); let's get
 # ourselves a newer version:
 
-echo 'deb http://httpredir.debian.org/debian/ jessie-backports main' >> \
+echo 'deb http://httpredir.debian.org/debian/ stretch-backports main' >> \
 	/etc/apt/sources.list.d/backports.list
 apt-get update
-apt-get -t jessie-backports install -y golang
+apt-get -t stretch-backports install -y golang
 
 # Needed for fetching most dependencies:
 apt-get install -y git
