3eb2a7cf60ff04eaba218dcd32ac2750555947ee
curbengh <43627182+curbengh@users.noreply.github.com>
2019-09-10 21:25:51 +0100

    fix: rename 'node' to 'node6'

---
+++ stacks/node6/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# When you change this file, you must take manual action. Read this doc:
+# - https://docs.sandstorm.io/en/latest/vagrant-spk/customizing/#setupsh
+
+set -euo pipefail
+# Install node.js
+
+# Discussion, issues and change requests at:
+#   https://github.com/nodesource/distributions
+#
+# Script to install the NodeSource Node.js 6.x repo onto a
+# Debian or Ubuntu system.
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Installing the NodeSource Node.js 6.x repo..."
+
+apt-get update
+apt-get install -qq apt-transport-https
+
+curl -sL https://deb.nodesource.com/setup_6.x | bash -
+
+# Actually install node
+apt-get install -qq nodejs git-core g++
+
+exit 0
