f825dc5b5087901a167ca44848f3f2d83307619e
Jacob Weisz <inbox@jacobweisz.com>
2021-04-10 15:06:03 -0500

    Rename EXE before uploading

--- release.sh
+++ release.sh
@@ -91,7 +91,8 @@ function create_github_release() {
     echo "Not creating GitHub release yet. Re-run with DRY_RUN=no in the environment."
     echo ""
   else
-    gh release create "$TAG_NAME" "$WINDOWS_EXE" -d -R sandstorm-io/vagrant-spk -n "$(python -c 's = open("CHANGELOG.md").read(); print s[:s.index("\n### ")-1]')"
+    mv "$WINDOWS_EXE" "vagrant-spk-setup-$TAG_NAME.exe"
+    gh release create "$TAG_NAME" "vagrant-spk-setup-$TAG_NAME.exe" -d -R sandstorm-io/vagrant-spk -n "$(python -c 's = open("CHANGELOG.md").read(); print s[:s.index("\n### ")-1]')"
   fi
 }
 
