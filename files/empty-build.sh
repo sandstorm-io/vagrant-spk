90914c78851444346bd22bbf6c22b00dc25fe001
Michael Nutt <michael@nuttnet.net>
2026-02-28 12:57:46 -0500

    extract regular files into files/ and have vagrant-spk and lima-spk reference them there

---
+++ files/empty-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+# This is a script that is run every time you call "vagrant-spk dev".
+# It is intended to do platform-and-repository-specific build steps.  You
+# might customize it to do any of the following, or more:
+# - for Python, prepare a virtualenv and pip install -r requirements.txt
+# - for PHP, call composer to retrieve additional packages
+# - for JS/CSS/SASS/LESS, compile, minify, or otherwise do asset pipeline work.
+# This particular script does nothing at present, but you should adapt it
+# sensibly for your package.
+
+exit 0
